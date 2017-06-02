79c2f80f9ccdf9c4b2309e7def284c49ed7db6f6
kllilizxc <541796726@qq.com>
2017-06-02 22:04:59 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/SPL_Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPL_Compiler.dir/main.cpp.obj"
+  "SPL_Compiler.pdb"
+  "SPL_Compiler.exe"
+  "SPL_Compiler.exe.manifest"
+  "libSPL_Compiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPL_Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
